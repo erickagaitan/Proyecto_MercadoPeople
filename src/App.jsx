843b09b533f4923244f451e5008f2ba4982f1c12
Esteban Rojas <e.rojassa@gmail.com>
2022-11-30 21:06:50 -0300

    

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { useState, useEffect } from 'react'
+// import Context from './context/context'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import Home from './views/public/Home'
+import Login from './views/public/Login'
 
 function App() {
   return (
     <div className="App">
-      
+
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/login" element={<Login />}>Login</Route>
+          <Route path="/" element={<Home />}>Home</Route>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/views/public/Login.jsx
@@ -0,0 +1,23 @@
+const Login = () => {
+
+    return (
+        <div className="container">
+            <div className="d-flex justify-content-center">
+                <div className="card col-lg-5 d-flex align-items-center mt-5 border-0">
+                    <h1>Iniciar Sesión</h1>
+                    <div className="row col-md-8 col-lg-6 col-8 justify-content-center"  style={{width: '18rem;'}}>
+                        <div>
+                            <label className="mt-3">Usuario:</label>
+                            <input className="form-control form-control-lg" type="text" />
+                            <label className="mt-3">Contraseña:</label>
+                            <input className="form-control form-control-lg" type="text" />
+                        </div>
+                        <button className="btn btn-success mt-3 mb-5 w-50">Acceder</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
